0cf13973222b18a8f03f11afb165c22f5637a427
Alfred Ali GÜlkan <alfredaligulkan@gmail.com>
2024-03-14 20:10:04 +0300

    Sıcaklık değerleri Celsius, Kelvin ve Fahrenheit cinsinden görünebiliyor.

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Sıcaklık_Değerleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Secim_Celcius = new System.Windows.Forms.Button();
+            this.Secim_Fahrenheit = new System.Windows.Forms.Button();
+            this.Secim_Kelvin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Cevir_Celcius = new System.Windows.Forms.Button();
+            this.Cevir_Fahrenheit = new System.Windows.Forms.Button();
+            this.Cevir_Kelvin = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(182, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Geri";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Secim_Celcius
+            // 
+            this.Secim_Celcius.Location = new System.Drawing.Point(32, 51);
+            this.Secim_Celcius.Name = "Secim_Celcius";
+            this.Secim_Celcius.Size = new System.Drawing.Size(75, 23);
+            this.Secim_Celcius.TabIndex = 1;
+            this.Secim_Celcius.Text = "Celcius (°C)";
+            this.Secim_Celcius.UseVisualStyleBackColor = true;
+            this.Secim_Celcius.Click += new System.EventHandler(this.Secim_Celcius_Click);
+            // 
+            // Secim_Fahrenheit
+            // 
+            this.Secim_Fahrenheit.Location = new System.Drawing.Point(171, 51);
+            this.Secim_Fahrenheit.Name = "Secim_Fahrenheit";
+            this.Secim_Fahrenheit.Size = new System.Drawing.Size(97, 23);
+            this.Secim_Fahrenheit.TabIndex = 2;
+            this.Secim_Fahrenheit.Text = "Fahrenheit (°F)";
+            this.Secim_Fahrenheit.UseVisualStyleBackColor = true;
+            this.Secim_Fahrenheit.Click += new System.EventHandler(this.Secim_Fahrenheit_Click);
+            // 
+            // Secim_Kelvin
+            // 
+            this.Secim_Kelvin.Location = new System.Drawing.Point(327, 51);
+            this.Secim_Kelvin.Name = "Secim_Kelvin";
+            this.Secim_Kelvin.Size = new System.Drawing.Size(75, 23);
+            this.Secim_Kelvin.TabIndex = 3;
+            this.Secim_Kelvin.Text = "Kelvin (°K)";
+            this.Secim_Kelvin.UseVisualStyleBackColor = true;
+            this.Secim_Kelvin.Click += new System.EventHandler(this.Secim_Kelvin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(28, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Cevir_Celcius
+            // 
+            this.Cevir_Celcius.Location = new System.Drawing.Point(32, 184);
+            this.Cevir_Celcius.Name = "Cevir_Celcius";
+            this.Cevir_Celcius.Size = new System.Drawing.Size(75, 23);
+            this.Cevir_Celcius.TabIndex = 6;
+            this.Cevir_Celcius.Text = "Celsius (°C)";
+            this.Cevir_Celcius.UseVisualStyleBackColor = true;
+            this.Cevir_Celcius.Click += new System.EventHandler(this.Cevir_Celcius_Click);
+            // 
+            // Cevir_Fahrenheit
+            // 
+            this.Cevir_Fahrenheit.Location = new System.Drawing.Point(171, 184);
+            this.Cevir_Fahrenheit.Name = "Cevir_Fahrenheit";
+            this.Cevir_Fahrenheit.Size = new System.Drawing.Size(97, 23);
+            this.Cevir_Fahrenheit.TabIndex = 7;
+            this.Cevir_Fahrenheit.Text = "Fahrenheit (°F)";
+            this.Cevir_Fahrenheit.UseVisualStyleBackColor = true;
+            this.Cevir_Fahrenheit.Click += new System.EventHandler(this.Cevir_Fahrenheit_Click);
+            // 
+            // Cevir_Kelvin
+            // 
+            this.Cevir_Kelvin.Location = new System.Drawing.Point(327, 184);
+            this.Cevir_Kelvin.Name = "Cevir_Kelvin";
+            this.Cevir_Kelvin.Size = new System.Drawing.Size(75, 23);
+            this.Cevir_Kelvin.TabIndex = 8;
+            this.Cevir_Kelvin.Text = "Kelvin (°K)";
+            this.Cevir_Kelvin.UseVisualStyleBackColor = true;
+            this.Cevir_Kelvin.Click += new System.EventHandler(this.Cevir_Kelvin_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(128, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Sıcaklık Birimi Seçiniz";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(109, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(263, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Hangi Sıcaklık Birimine Çevrilsin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 268);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Cevir_Kelvin);
+            this.Controls.Add(this.Cevir_Fahrenheit);
+            this.Controls.Add(this.Cevir_Celcius);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Secim_Kelvin);
+            this.Controls.Add(this.Secim_Fahrenheit);
+            this.Controls.Add(this.Secim_Celcius);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Secim_Celcius;
+        private System.Windows.Forms.Button Secim_Fahrenheit;
+        private System.Windows.Forms.Button Secim_Kelvin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Cevir_Celcius;
+        private System.Windows.Forms.Button Cevir_Fahrenheit;
+        private System.Windows.Forms.Button Cevir_Kelvin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sıcaklık_Değerleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            Secim_Celcius.Visible = true;
+            Secim_Fahrenheit.Visible = true;
+            Secim_Kelvin.Visible = true;
+
+            Cevir_Kelvin.Visible = true;
+            Cevir_Fahrenheit.Visible = true;
+            Cevir_Celcius.Visible = true;
+
+            textBox1.Text = null;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            textBox1.Text = "0";
+        }
+
+        private void Secim_Celcius_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Celcius (°C)";
+            label1.Visible = true;
+
+            Secim_Celcius.Visible = false;
+            Secim_Fahrenheit.Visible = false;
+            Secim_Kelvin.Visible = false;
+
+            Cevir_Celcius.Visible = false;
+        }
+
+        private void Secim_Fahrenheit_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Fahrenheit (°F)";
+            label1.Visible = true;
+
+            Secim_Celcius.Visible = false;
+            Secim_Fahrenheit.Visible = false;
+            Secim_Kelvin.Visible = false;
+
+            Cevir_Fahrenheit.Visible = false;
+        }
+
+        private void Secim_Kelvin_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Kelvin (°K)";
+            label1.Visible = true;
+
+            Secim_Celcius.Visible = false;
+            Secim_Fahrenheit.Visible = false;
+            Secim_Kelvin.Visible = false;
+
+            Cevir_Kelvin.Visible = false;
+        }
+
+        private void Cevir_Fahrenheit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number1 = Convert.ToInt32(textBox1.Text);
+
+                if (label1.Text == "Celcius (°C)")
+                {
+                    double sonuc = number1 * 1.8 + 32;
+                    MessageBox.Show(Convert.ToString(number1) + " Celcius (°C) " + sonuc + " Fahrenheit (°F) etmektedir.");
+                }
+
+                if (label1.Text == "Kelvin (°K)")
+                {
+                    double sonuc = (number1 - 273.15) * 1.8 + 32;
+                    MessageBox.Show(Convert.ToString(number1) + " Kelvin (°K) " + sonuc + " Fahrenheit (°F) etmektedir.");
+                }
+
+                label1.Visible = false;
+                Secim_Celcius.Visible = true;
+                Secim_Fahrenheit.Visible = true;
+                Secim_Kelvin.Visible = true;
+
+                Cevir_Kelvin.Visible = true;
+                Cevir_Fahrenheit.Visible = true;
+                Cevir_Celcius.Visible = true;
+
+                textBox1.Text = null;
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Lütfen geçerli bir değer giriniz!!!");
+            }           
+        }
+
+        private void Cevir_Kelvin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number1 = Convert.ToInt32(textBox1.Text);
+
+                if (label1.Text == "Celcius (°C)")
+                {
+                    double sonuc = number1 + 273.15;
+                    MessageBox.Show(Convert.ToString(number1) + " Celcius (°C) " + sonuc + " Kelvin (°K) etmektedir.");
+                }
+
+                if (label1.Text == "Fahrenheit (°F)")
+                {
+                    double sonuc = (number1 - 32) / 1.8 + 273.15;
+                    MessageBox.Show(Convert.ToString(number1) + " Fahrenheit (°F) " + sonuc + " Kelvin (°K) etmektedir.");
+                }
+
+                label1.Visible = false;
+                Secim_Celcius.Visible = true;
+                Secim_Fahrenheit.Visible = true;
+                Secim_Kelvin.Visible = true;
+
+                Cevir_Kelvin.Visible = true;
+                Cevir_Fahrenheit.Visible = true;
+                Cevir_Celcius.Visible = true;
+
+                textBox1.Text = null;
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Lütfen geçerli bir değer giriniz!!!");
+            }           
+        }
+
+        private void Cevir_Celcius_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number1 = Convert.ToInt32(textBox1.Text);
+
+                if (label1.Text == "Fahrenheit (°F)")
+                {
+                    double sonuc = (number1 - 32) / 1.8;
+                    MessageBox.Show(Convert.ToString(number1) + " Fahrenheit (°F) " + sonuc + " Celcius (°C) etmektedir.");
+                }
+
+                if (label1.Text == "Kelvin (°K)")
+                {
+                    double sonuc = (number1 - 273.15);
+                    MessageBox.Show(Convert.ToString(number1) + " Kelvin (°K) " + sonuc + " Celcius (°C) etmektedir.");
+                }
+
+                label1.Visible = false;
+                Secim_Celcius.Visible = true;
+                Secim_Fahrenheit.Visible = true;
+                Secim_Kelvin.Visible = true;
+
+                Cevir_Kelvin.Visible = true;
+                Cevir_Fahrenheit.Visible = true;
+                Cevir_Celcius.Visible = true;
+
+                textBox1.Text = null;
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Lütfen geçerli bir değer giriniz!!!");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
